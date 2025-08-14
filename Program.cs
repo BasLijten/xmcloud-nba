90d226ccedf1d2c920f5e26a0e56b7978d717dc2
Bas Lijten <bas.lijten@achmea.nl>
2025-08-14 21:48:34 +0200

    added random value

--- Program.cs
+++ Program.cs
@@ -7,24 +7,14 @@ app.MapPost("/GetNBA", ([FromBody] NbaRequest? request) =>
 {
     var id = request?.Id;
     NbaResponse response = null;
-    if (id == "990e1b60-f262-4b74-a949-8abe7f519929") { response = new NbaResponse(id, "auto"); }
-    else response = new NbaResponse(id, GenerateRandomNBAResult());
+    var choices = new[] { "reis", "auto", "aanhanger", "duikboot", "hypotheek" };
+    var nba = choices[System.Random.Shared.Next(choices.Length)];
+    if (id == "bas") { response = new NbaResponse(id, "auto"); }
+    else { response = new NbaResponse(id, nba); }
     return Results.Ok(response);
-
-    // generate local function to return empty string
-    string GenerateRandomNBAResult()
-{
-    var results = new[] { "auto", "reis", "hypotheek", "aanhanger", "duikboot" };
-    var random = new Random();
-    return results[random.Next(results.Length)];
-}
-
-})();
+});
 
 app.Run();
 
 public record NbaRequest(string? Id);
 public record NbaResponse(string? Id, string NBA);
-
-// generate function which returns one of the following:
-// auto, reis, hypotheek, aanhanger, duikboot
