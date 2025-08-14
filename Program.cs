606bd2fed482b67a471f7583ea81e51a61a7b87f
Bas Lijten <bas.lijten@achmea.nl>
2025-08-14 21:26:35 +0200

    update to code

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ app.MapPost("/GetNBA", ([FromBody] NbaRequest? request) =>
 {
     var id = request?.Id;
     NbaResponse response = null;
-    if (id == "bas") { response = new NbaResponse(id, "auto"); }
+    if (id == "990e1b60-f262-4b74-a949-8abe7f519929") { response = new NbaResponse(id, "auto"); }
     if (id == "stefan") { response = new NbaResponse(id, "reis"); }
     return Results.Ok(response);
 });
